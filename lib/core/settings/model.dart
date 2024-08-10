2bf6861d2ed2f9ba4133fea5d136259eaf343260
Yaroslav Pronin <proninyaroslav@gmail.com>
2024-08-10 12:02:25 +0300

    Update copyright date

--- lib/core/settings/model.dart
+++ lib/core/settings/model.dart
@@ -1,4 +1,4 @@
-// Copyright (C) 2021 Yaroslav Pronin <proninyaroslav@mail.ru>
+// Copyright (C) 2021-2024 Yaroslav Pronin <proninyaroslav@mail.ru>
 // Copyright (C) 2021 Insurgo Inc. <insurgo@riseup.net>
 //
 // This file is part of LibreTrack.
